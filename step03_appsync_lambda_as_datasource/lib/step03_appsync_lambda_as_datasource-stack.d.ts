47c7218016486b3634a015683141e11c4d492b48
owaisahmad870 <88275066+owaisahmad870@users.noreply.github.com>
2021-08-09 02:42:34 +0500

    Delete step03_appsync_lambda_as_datasource-stack.d.ts

--- step03_appsync_lambda_as_datasource/lib/step03_appsync_lambda_as_datasource-stack.d.ts
+++
@@ -1,4 +0,0 @@
-import * as cdk from '@aws-cdk/core';
-export declare class Step03AppsyncLambdaAsDatasourceStack extends cdk.Stack {
-    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps);
-}
